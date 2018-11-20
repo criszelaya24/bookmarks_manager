692f30dfb63b1b49a0dd73a3d4be9119f41ec437
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-20 10:51:47 +0000

    Making the connection with our DB, extracting the information and passing the tests

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -9,7 +9,7 @@ feature 'Viewing bookmarks' do
 	scenario 'A user can see bookmarks' do
 		visit('/bookmarks')
 	  	expect(page).to have_content "http://www.makersacademy.com"
-	    expect(page).to have_content "http://www.destroyallsoftware.com"
+	    expect(page).to have_content "http://www.askjeeves.com"
 	    expect(page).to have_content "http://www.google.com"
 	end
 end
